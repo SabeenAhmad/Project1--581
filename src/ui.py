4f97702280ac793fe5b125e7e2a68ad035c51cd7
kwhuber <kaden.huber@gmail.com>
2025-08-31 18:05:17 -0500

    created rough sketch of ui.py file

---
+++ src/ui.py
@@ -0,0 +1,16 @@
+# UI file
+
+def start_screen(): 
+    #move over from game.py
+
+def end_screen(): 
+
+def render_game(): 
+
+def render_status(): 
+
+def render_board(): 
+
+def ask_for_input(): 
+
+
