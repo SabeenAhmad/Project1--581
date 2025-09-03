6baa5cfff3addac724ece79fd3958144a9f39a26
Tanusakaray <tsakaray3@gmail.com>
2025-09-02 22:28:54 -0500

    Add toggle_flag method to Game

--- src/game.py
+++ src/game.py
@@ -22,6 +22,28 @@ class Game:
     def game_loop(self):
         print("\n--- Game started! ---")
         self.board.print_board(self.playing_state)
+    
+    def toggle_flag(self, r, c):
+        if not self.board.in_bounds(r, c):
+            return 'INVALID'
 
+        cell_state = self.board.state[r][c]
 
+        # cannot flag an uncovered cell
+        if cell_state == 'UNCOVERED':
+            return 'INVALID'
+
+        # unflag
+        if cell_state == 'FLAG':
+            self.board.state[r][c] = 'COVERED'
+            self.flags_remaining += 1
+            return 'UNFLAGGED'
         
+        # place flag
+        if self.flags_remaining == 0:
+            return 'NO_FLAGS'
+
+        self.board.state[r][c] = 'FLAG'
+        self.flags_remaining -= 1
+        return 'FLAGGED'
+            
