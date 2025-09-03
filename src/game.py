19f5d7010ec06f3951c330e810a5031daf06c419
Tanusakaray <tsakaray3@gmail.com>
2025-09-03 11:47:05 -0500

    removed stuff from game

--- src/game.py
+++ src/game.py
@@ -16,44 +16,8 @@ class Game:
         self.flags_remaining = mines_total
         self.board = Board(self.mine_total)
         self.first_move = True
-<<<<<<< HEAD
-        self.start_screen()
-    
-    def start_screen(self):
-        input("=== Welcome to Minesweeper ===\nPress ENTER to start...")
-        self.game_loop()
-    def game_loop(self):
-        print("\n--- Game started! ---")
-        self.board.print_board(self.playing_state)
-    
-    def toggle_flag(self, r, c):
-        if not self.board.in_bounds(r, c):
-            return 'INVALID'
-
-        cell_state = self.board.state[r][c]
-
-        # cannot flag an uncovered cell
-        if cell_state == 'UNCOVERED':
-            return 'INVALID'
-
-        # unflag
-        if cell_state == 'FLAG':
-            self.board.state[r][c] = 'COVERED'
-            self.flags_remaining += 1
-            return 'UNFLAGGED'
-        
-        # place flag
-        if self.flags_remaining == 0:
-            return 'NO_FLAGS'
-
-        self.board.state[r][c] = 'FLAG'
-        self.flags_remaining -= 1
-        return 'FLAGGED'
-            
-=======
   
         # Moved UI files to ui.py class and made a class in that ui.py so we can call upon methods defined there. 
         self.ui = UI(self, self.board)
         self.ui.start_screen()
  
->>>>>>> main
